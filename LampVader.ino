f59d1611b3c69c94d481d02cbd823b65d4dbf0d3
balsimpson <thirdi@gmail.com>
2016-11-27 04:30:38 +0530

    update

---
+++ LampVader.ino
@@ -0,0 +1,64 @@
+
+void loop() {
+
+
+
+
+
+
+
+}
+
+
+
+
+On Startup
+- startup sequence
+
+Check values
+
+On Notification
+- change sequence/ palette
+
+Define Sequences
+- startup
+  - random color palette swirl
+  - settle on last pattern/palette
+
+- notification
+  - 'there is a notification' swirl
+  - specific notification pattern/palette
+  - 'that was a notification' swirl
+  - back to current pattern/palette
+
+  Define Palettes
+  - Notifications
+    - Service Icon color
+
+  - Timers
+    - Sunrise/ Sunset/ Moonrise/ Rain
+
+  - Gauges
+
+
+
+
+  /* Colors
+   * CornflowerBlue
+   * Coral - Orange
+   * Crimson
+   * DeepPink
+   * BlueViolet
+   * CadetBlue
+   * DeepSkyBlue
+   * Cyan
+   * DarkTurquoise
+   * DarkCyan
+   * Aquamarine
+   * BlanchedAlmond
+   * Chartreuse - Bright Green
+   * DarkOliveGreen
+   * DarkSeaGreen
+   * Cornsilk
+   * DarkMagenta
+   */
